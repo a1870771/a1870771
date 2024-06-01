97f0151ac1024c6281cfcbf3441b86bb70c7f21f
Jack Larkin <a1870771@adelaide.edu.au>
2024-06-01 23:52:10 +0930

    init commit

--- pages/index.js
+++ pages/index.js
@@ -5,60 +5,136 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Adelaide Encyclopaedia</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Encyclopaedia of Adelaide Bands, Solo Artists and Duos
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          By Darcy, 2024 (Website by Jack)
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    
+        <div className={styles.description}>
+          <ul>
+            <li>A</li>
+            <li>Alexander Black</li>
+            <li>B</li>
+            <li>Babyteeth</li>
+            <li>Big Sloe</li>
+            <li>Birds Are Spies</li>
+            <li>Blush Response</li>
+            <li>Blue Lucy</li>
+            <li>Busseys</li>
+            <li>C</li>
+            <li>Chelsea Manor</li>
+            <li>Choosing Sides</li>
+            <li>Clover</li>
+            <li>Colter</li>
+            <li>Confession Tapes</li>
+            <li>Connections</li>
+            <li>D</li>
+            <li>Day For Caroline</li>
+            <li>Dandy Buzzkills</li>
+            <li>Dainty Morsels</li>
+            <li>Destinezia</li>
+            <li>E</li>
+            <li>East Ave</li>
+            <li>Energy Angel</li>
+            <li>Exit Plan</li>
+            <li>F</li>
+            <li>Fair Call</li>
+            <li>February Clouds</li>
+            <li>Free Drinks</li>
+            <li>Full Cream</li>
+            <li>G</li>
+            <li>George Alice</li>
+            <li>H</li>
+            <li>Homecourt</li>
+            <li>Haze Daze</li>
+            <li>I</li>
+            <li>J</li>
+            <li>Jongo Bones & the Barefoot Bandits</li>
+            <li>K</li>
+            <li>Kaleidoscopickumquats</li>
+            <li>Keyan</li>
+            <li>Knight Over Day</li>
+            <li>L</li>
+            <li>Lily Rose</li>
+            <li>Live Slug Reaction</li>
+            <li>Lola</li>
+            <li>Looch</li>
+            <li>Lumens</li>
+            <li>M</li>
+            <li>MACEY</li>
+            <li>Masie</li>
+            <li>Marlin Kites</li>
+            <li>Matilda Pearl</li>
+            <li>Molly Rocket</li>
+            <li>Moshpit Girlfriend</li>
+            <li>Mum Thinks Blue</li>
+            <li>My Cherrie</li>
+            <li>N</li>
+            <li>Not For Humans</li>
+            <li>O</li>
+            <li>Oscar The Wild</li>
+            <li>P</li>
+            <li>Pelvis</li>
+            <li>Pink Duke</li>
+            <li>Placement</li>
+            <li>Purée</li>
+            <li>Q</li>
+            <li>R</li>
+            <li>Ricky Albeck</li>
+            <li>S</li>
+            <li>Samuelson</li>
+            <li>saschasaidso</li>
+            <li>Satin Sun</li>
+            <li>Signals</li>
+            <li>Somnium</li>
+            <li>Stellar</li>
+            <li>Stormy Lou</li>
+            <li>St. Loki</li>
+            <li>Sugartongue</li>
+            <li>Sunsick Daisy</li>
+            <li>Swapmeet</li>
+            <li>T</li>
+            <li>Taylaa</li>
+            <li>Tapleys Hill</li>
+            <li>Tell Mama</li>
+            <li>The Danger Dolfins</li>
+            <li>The Fuss</li>
+            <li>The Genevieves</li>
+            <li>The Hazys</li>
+            <li>The Lizards</li>
+            <li>The Max Headroom</li>
+            <li>The Mondays</li>
+            <li>The Munch</li>
+            <li>The Sundials</li>
+            <li>The Tullamarines</li>
+            <li>The Vains</li>
+            <li>The 745</li>
+            <li>Toob Scoots</li>
+            <li>Townhouse</li>
+            <li>Towns</li>
+            <li>Tonix</li>
+            <li>TV Therapy</li>
+            <li>U</li>
+            <li>V</li>
+            <li>Violet Harlott</li>
+            <li>W </li>
+            <li>Weekend Rage</li>
+            <li>Whitebulb</li>
+            <li>Witch Spit</li>
+            <li>X</li>
+            <li>Y</li>
+            <li>Z</li>
+          </ul>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
 
       <style jsx>{`
         main {
